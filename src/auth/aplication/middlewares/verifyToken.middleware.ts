e10e8bc5e212343342db1f460cb6df33c2913f84
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-04-17 00:48:23 -0600

    Corregir errores en el middleware de verificación de token y en el repositorio de MySQL

--- src/auth/aplication/middlewares/verifyToken.middleware.ts
+++ src/auth/aplication/middlewares/verifyToken.middleware.ts
@@ -6,20 +6,33 @@ export class VerifyTokenService {
   constructor(
     private readonly tokenRepository: TokenRepository,
     private readonly especialistaRepository: EspecialistaRepository
-  ) {}
+  ) { }
   async run(req: Request, res: Response, next: NextFunction) {
     try {
-      let token = req.get("Authorization");
-      const especialista = await this.especialistaRepository.findByEmailPremium(
+      const especialistaPrem = await this.especialistaRepository.findByEmailPremium(
         req.body.correo
       );
-      console.log(especialista);
+      const infoEspecialistaPrem = especialistaPrem as any;
+      if (infoEspecialistaPrem.cantidad_citas > 100) {
+        if (!infoEspecialistaPrem.fecha_inicio || !infoEspecialistaPrem.fecha_fin) {
+          return res.status(401).send({
+            message: "No puedes agendar más citas, ya cumpliste las 100 citas gratuitas, compra un plan premium",
+          });
+        }
+        if (new Date(infoEspecialistaPrem.fecha_fin) < new Date()) {
+          return res.status(401).send({
+            message: "No puedes agendar más citas, ya acabó tu plan, compra un plan premium nuevo",
+          });
+        }
+      }
+      let token = req.get("Authorization");
       if (token) {
         token = token.substring(7);
         this.tokenRepository.verifyToken(token);
         next();
+      } else {
+        return res.status(401).send({ message: "Token inexistente" });
       }
-      return res.status(401).send({ message: "Token inexistente" });
     } catch (err: any) {
       return res.status(401).send({
         message: "Token invalido",
--- src/specialist/infraestructure/DB/mysql.repository.ts
+++ src/specialist/infraestructure/DB/mysql.repository.ts
@@ -1,6 +1,7 @@
 import { db } from "../../../shared/application/mysqlConnection";
 import { EspecialistaRepository } from "../../domain/repository/especialista.repository";
 import { Especialista } from "../../domain/entities/especialista.objet";
+import { RowDataPacket } from "mysql2";
 
 export class MysqlEspecialistaRepository implements EspecialistaRepository {
     async create(especialista: Especialista): Promise<Especialista> {
@@ -14,20 +15,20 @@ export class MysqlEspecialistaRepository implements EspecialistaRepository {
     async findByEmail(correo: string): Promise<Especialista> {
         const query = "select * from especialistas where correo = ?";
         return db.execute(query, [correo]).then((res: any) => {
-            console.log(res[0]);
             return res[0][0] as Especialista;
         });
     }
 
     async findByEmailPremium(correo: string): Promise<Especialista> {
         const query = " SELECT COUNT(c.id_consultas) AS cantidad_citas FROM armcheckdb.consultas c JOIN armcheckdb.pacientes pa ON c.id_paciente = pa.id_persona JOIN armcheckdb.especialistas e ON pa.id_especialista = e.id_especialista WHERE e.correo  = ? ";
-        const query2 = "SELECT p.* FROM armcheckdb.pagos AS p JOIN armcheckdb.especialistas AS e ON p.id_especialista = e.id_especialista WHERE e.correo = ? ORDER BY p.fecha_fin DESC LIMIT 1"
+        const query2 = "SELECT p.fecha_inicio, p.fecha_fin FROM armcheckdb.pagos AS p JOIN armcheckdb.especialistas AS e ON p.id_especialista = e.id_especialista WHERE e.correo = ? ORDER BY p.fecha_fin DESC LIMIT 1"
         const result1 = await db.execute(query, [correo]);
         const result2 = await db.execute(query2, [correo]);
         const especialista = {
-            ...result1[0],
-            ...result2[0]
-        } as unknown as Especialista;
+            "cantidad_citas": (result1[0] as RowDataPacket[])[0].cantidad_citas,
+            "fecha_inicio": (result2[0] as RowDataPacket[])[0].fecha_inicio,
+            "fecha_fin": (result2[0] as RowDataPacket[])[0].fecha_fin
+        }as unknown as Especialista;
         return especialista;
     }
     
